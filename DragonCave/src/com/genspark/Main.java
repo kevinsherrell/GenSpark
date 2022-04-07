1467fecb261285bedd2cb302bbada69bc6244b69
kevinsherrell <k.sherrell.1028@gmail.com>
2022-04-07 14:56:18 -0600

    dragon cave try catch - project 3

--- DragonCave/src/com/genspark/Main.java
+++ DragonCave/src/com/genspark/Main.java
@@ -6,9 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        System.out.println("You are in a land full of dragons. In front of you,\nyou see two caves. In one cave, the dragon is friendly\nand will share his treasure with you. The other dragon\nis greedy and hungry and will eat you on sight.\n Which cave will you go into? (1 or 2 )");
+        try {
+            runGame();
+        } catch (Exception e) {
+            System.out.println("Please enter a valid response");
+            runGame();
+        }
+
+    }
+
+    public static void runGame() {
         Scanner scanner = new Scanner(System.in);
         int input;
-        System.out.println("You are in a land full of dragons. In front of you,\nyou see two caves. In one cave, the dragon is friendly\nand will share his treasure with you. The other dragon\nis greedy and hungry and will eat you on sight.\n Which cave will you go into? (1 or 2 )");
         input = scanner.nextInt();
         System.out.println("You approach the cave...\nIt is dark and spooky...\n");
         if (input == 1) {
